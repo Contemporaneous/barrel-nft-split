7a81dfc5461eafa8bdab7ca5a532ddfa5a4f0c5b
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-06-04 17:19:38 +1000

    Add minting functionality + test

--- contracts/Barrel.sol
+++ contracts/Barrel.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+import { Base64 } from "./libraries/Base64.sol";
+
 import "hardhat/console.sol";
 contract Barrel is ERC721 {
 
@@ -11,9 +13,73 @@ contract Barrel is ERC721 {
     uint256 public totalSupply;
     uint256 public expectedSupply;
 
-constructor(uint256 _totalSupply, uint256 _expectedSupply) payable ERC721("Barrel","BBL") {
+    using Counters for Counters.Counter;
+    Counters.Counter private tokenIds;
+
+    struct tokenDetails {
+        uint256 tokenId;
+        uint256 mintTime;
+    }
+
+    mapping(uint256 => tokenDetails) private tokenDetailsMap;
+
+    constructor(uint256 _totalSupply, uint256 _expectedSupply) ERC721("Barrel","BBL") {
         owner = payable(msg.sender);
         totalSupply = _totalSupply;
         expectedSupply = _expectedSupply;
     }
+
+    function getPrice() internal pure returns (uint256) {
+        //Fixed price (Implement dutch auction later)
+        return 1*10**16;
+    }
+
+    function generateNFT() public payable{
+        uint256 price = getPrice();
+        require(msg.value >= price, "You need to pay the correct price");
+        require(tokenIds.current() < totalSupply, "Allocation exhausted");
+
+        uint256 newItemId = tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+
+        //Store details
+        tokenDetailsMap[newItemId] = tokenDetails(newItemId, block.timestamp);
+
+        // Increment the counter for when the next NFT is minted.
+        tokenIds.increment();
+    }
+
+    function generateURI(uint256 id) public view returns (string memory) {
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "Barrel Portion (',Strings.toString(id),')", "description": "A Porion of the barrel.",',
+                        '"attributes": [ {"trait_type": "Mint Time", "value": "',Strings.toString(tokenDetailsMap[id].mintTime),'" }',
+                        ']}'
+                    )
+                )
+            )
+        );
+
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+            
+        return finalTokenUri;
+    }
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) { 
+        return generateURI(_tokenId);
+    }
+
+    function getPosition(uint256 _tokenId) public view returns (bool, uint256) {
+        if (_tokenId > expectedSupply) {
+            return (false, _tokenId - expectedSupply);
+        } else {
+            return (true, expectedSupply-_tokenId);
+        }
+    }
+
 }
--- test/Barrel.test.js
+++ test/Barrel.test.js
@@ -1,10 +1,10 @@
-const { expect } = require("chai");
+const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 
 let pif;
 let accounts;
-const totalSupply = 500;
-const expectedSupply = 400;
+const totalSupply = 5;
+const expectedSupply = 3;
 
 beforeEach(async () => {
     accounts = await ethers.getSigners();
@@ -23,4 +23,37 @@ describe("Deployment", function () {
   it("Should deploy with correct owner", async function () {
     expect(await barrel.owner()).to.equal(accounts[0].address);
   });
+});
+
+describe("Minting", function () {
+    it("Should Mint to correct address", async function () {
+        await barrel.connect(accounts[1]).generateNFT({value: ethers.utils.parseEther("0.01")});
+        expect(await barrel.ownerOf(0)).to.equal(accounts[1].address);
+    });
+
+    it("Should not exceed total supply", async function() {
+        const func = async () => {
+            for (let i = 0; i = totalSupply; i++) {
+                let fred = await barrel.generateNFT({value: ethers.utils.parseEther("0.01")});
+                fred.wait();
+            }
+        };
+        
+        //BAD TEST - Why does .to.throw not work?
+        try {
+            await func();
+            expect(1).to.equal(0);
+        } catch (e) {
+            expect(1).to.equal(1)
+        }
+    });
+
+    it("Should Show Correct Position", async function () {
+        await barrel.connect(accounts[1]).generateNFT({value: ethers.utils.parseEther("0.01")});
+        res = await barrel.getPosition(0);
+
+        expect(res[0]).to.equal(true);
+        expect(res[1]).to.equal(expectedSupply);
+        
+    });
 });
